7d88a0be738c2ebbdce7d3fdc8dabcb7177e6d4d
sheshank <sheshank3336@gmail.com>
2026-05-31 16:59:31 -0500

    Bump version to 14, fix deprecated track param in workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.schedulo2.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 11
-    versionName = "11.0"
+    versionCode = 14
+    versionName = "14.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
